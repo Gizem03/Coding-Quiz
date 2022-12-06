3f8a53ef179d078750da9c6a18ff6acd6fa4a4fb
Gizem Unlu <unlu.sgizem@gmail.com>
2022-12-06 18:58:14 +0000

    added questions

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,28 @@
+
+var questions = [
+    {
+        title: "What is the acronym for the Document Object Model?"
+        choices: ['OBJ', 'JS', 'DOM', 'ARR'],
+        answer: 'DOM'
+    },
+    {
+        title: "What is the useful tool for DRYing up code?"
+        choices: ['localStorage', 'strings', 'comments', 'functions'],
+        answer: 'functions'
+    },
+    {
+        title: "Arrays in JavaScript can be used to store:"
+        choices: ['number and strings', 'other arrays', 'booleans', 'all of the above'],
+        answer: 'all of the above'
+    },
+    {
+        title: "Commonly used datatypes DO NOT include:"
+        choices: ['strings', 'booleans', 'alerts', 'numbers'],
+        answer: 'alerts'
+    },
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:"
+        choices: ['terminal', 'console.log', 'for loops', 'JavaScript'],
+        answer: 'console.log'
+    }
+]
